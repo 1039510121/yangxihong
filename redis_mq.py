e18f989d316dda921112ec8e0082871a71e2e093
meiweimin <1039510121@qq.com>
2020-07-15 18:23:36 +0800

    update redis_mq

--- redis_mq.py
+++ redis_mq.py
@@ -1,6 +1,26 @@
 import redis
+tt=1024
+date=tt.to_bytes(length=4, byteorder='little', signed=True)
+buf=bytearray()
+buf.extend(date)
+print(buf)
+print(buf)
+print(date[0])
+print(date[1])
+
+st="1234"
+st.encode()
+buf.extend(st.encode())
+print(buf)
+
+
 Queue="test"
-redisPool = redis.ConnectionPool(host='localhost', port=6379, db=8)
+redisPool =redis.ConnectionPool(host='localhost', port=6379, db=8)
 client = redis.Redis(connection_pool=redisPool)
-client.rpush(Queue,"123")
-print(client.rpop(Queue))
+client.rpush(Queue,bytes(buf))
+t=client.rpop(Queue)
+print(type(t))
+print(len(t))
+for i in range(0,len(t)):
+    print(t[i])
+
